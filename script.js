84d7f96f392b1624629573e1653caafdefec3db7
Ahamad Rishad <aahamadrishad@gmail.com>
2024-01-12 11:00:09 -0500

    added JS to login and sighup page

---
+++ script.js
@@ -0,0 +1,31 @@
+const message = document.getElementById('message');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const btn = document.getElementById('btn');
+
+  const regX = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g
+
+btn.addEventListener('click',(e)=>{
+ 
+  e.preventDefault();
+ 
+  if(email.value.trim() == ''){
+    message.innerText = "email is Null";
+    message.style.display = 'block';
+  }
+  else if (!email.value.match(regX)){
+    message.innerText = "email is not proper"; 
+    message.style.display = 'block';
+  }
+  else if (password.value.length<8){
+    message.innerText = "Password should be 8 character";
+    message.style.display = 'block'; 
+  }
+  else{
+    
+    message.classList.remove('alert-danger');
+    message.classList.add('alert-success')
+    message.innerText = "success";
+    message.style.display = 'block';
+  }
+})
